68f17ca234f3a82622e4a61639537d4955d8b049
LuHZhan <1393454295@qq.com>
2025-02-14 00:31:57 +0800

    Add LuExperiment Plugin

---
+++ Plugins/LuExperiment/Source/LuExperiment/Private/LuExperimentBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LuExperimentBPLibrary.h"
+#include "LuExperiment.h"
+
+ULuExperimentBPLibrary::ULuExperimentBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float ULuExperimentBPLibrary::LuExperimentSampleFunction(float Param)
+{
+	return -1;
+}
+
